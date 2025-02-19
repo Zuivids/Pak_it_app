f4d5748c528c2d2b018cd7553a942107e5d833c8
Martins Z <112480628+Zuivids@users.noreply.github.com>
2025-02-19 22:44:46 +0200

    Spring boot initial commit

---
+++ src/main/java/lv/pakit/PakItAppApplication.java
@@ -0,0 +1,13 @@
+package lv.pakit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakItAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/pakit/PakItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.pakit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
