8a566d3f4b6f6deb7f0d25ff7ec1b057db745b3b
Martins Z <112480628+Zuivids@users.noreply.github.com>
2025-05-29 22:12:20 +0300

    Added Flyway V2 to populate demo data.

---
+++ src/main/resources/db/migration/V2__insert_demo_data.sql
@@ -0,0 +1,46 @@
+
+INSERT INTO client (email, full_name, phone_number)
+VALUES
+  ('janis.berzins@inbox.lv', 'Jānis Berzins', '+37144448888'),
+  ('valids.ozolins@inbox.lv', 'Valdis Ozolins', '+37133338888'),
+  ('karlis.kalnins@inbox.lv', 'Kārlis Kalniņš', '+37122228888');
+
+INSERT INTO `user` (username, password, phone_number, email)
+VALUES
+  ('admin', 'password', '+37111111111','admin@admin.lv');
+
+INSERT INTO commodity (commodity_code, description)
+VALUES
+  ('0000000001', 'Table'),
+  ('0000000002', 'Chair'),
+  ('0000000003', 'Medicament'),
+  ('0000000004', 'Tire'),
+  ('0000000005', 'Window'),
+  ('0000000006', 'Monitor'),
+  ('0000000007', 'Boots'),
+  ('0000000008', 'Bag'),
+  ('0000000009', 'Used Bag'),
+  ('0000000010', 'Used Boots'),
+  ('0000000011', 'Used Monitor');
+
+INSERT INTO declaration (client_id, `date`, identifier_code, sender_name, sender_address,
+sender_country_code,sender_phone_number, receiver_name, receiver_address, receiver_country_code,
+receiver_phone_number,total_value, total_weight)
+VALUES
+  (1,'20250529','1MAY2025','Toms Bērziņš','Ventspils iela 77','LV','+3711111111',
+  'Elizabeth Oak','London street 77','UK','+449876543',191.5,27.5),
+  (2,'20250528','2MAY2025','Pēteris Bērziņš','Rīgas iela 77','LV','+37122222222',
+  'Mark Grass','Bridge street 41','UK','+442222222',1311.49,93.05),
+  (3,'20250527','3MAY2025','John Wood','Crown street 1','UK','+4433333333',
+  'Miks Bērziņš','Tukuma iela 77','LV','+37133333333',94.72,14.5);
+
+INSERT INTO package_item (commodity_id,declaration_id,net_weight,quantity,used,`value`)
+ VALUES
+ (1,1,15.25,1,FALSE,120.50),
+ (7,1,5,1,FALSE,20.50),
+ (4,2,80.55,4,FALSE,1200),
+ (5,2,5.25,1,FALSE,10.59),
+ (8,2,7.25,2,FALSE,100.90),
+ (9,1,7.25,4,TRUE,50.50),
+ (10,3,7.25,1,TRUE,14.50),
+ (11,3,7.25,1,TRUE,80.22);
